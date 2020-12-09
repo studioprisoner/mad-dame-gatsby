65305336c34778172fe7076efd95f2d1bcea5498
Josh Illichmann <josh@studioprisoner.com>
2020-12-09 14:55:47 +1100

    fixing icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         display: `minimal-ui`,
+        icon: `src/images/mad-dame.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
