f38e11a33fe7cc1ebaec220e3f1a7b1a92738fac
Josh Illichmann <josh@studioprisoner.com>
2020-12-09 14:44:08 +1100

    fixed up gatsby icon and removed some theme details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Mad Dame`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Mad Dame`,
     author: `Joshua Illichmann`,
   },
   plugins: [
@@ -37,10 +37,7 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
