927a44c1fd72cef4425c8defabfa1ab705be7c3e
Josh Illichmann <josh@studioprisoner.com>
2022-02-05 11:39:46 +1100

    adding power portfolio

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import DamedMagazineImg from "../gallery/cover_img/power-portfolio.jpg"
 import DamedMagazineImg from "../gallery/cover_img/damed-magazine.jpg"
 import GlitterImg from "../gallery/cover_img/glitter.jpg"
 import NudedDeadImg from "../gallery/cover_img/nude-dead.jpg"
@@ -16,6 +17,21 @@ const Gallery = () => (
         <div className="bg-white">
             <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-16">
                 <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+                    <Link to="/power-portfolio">
+                        <li>
+                            <div className="space-y-4">
+                                <div className="aspect-w-3 aspect-h-2">
+                                    <img className="object-cover shadow-lg rounded-lg" src={PowerPortfolioImg} alt="Damed Magazine" />
+                                </div>
+
+                                <div className="space-y-2">
+                                    <div className="text-2xl leading-6 font-bold space-y-1 uppercase">
+                                        <h3>Power Portfolio</h3>
+                                    </div>
+                                </div>
+                            </div> 
+                        </li>
+                    </Link>
                     <Link to="/damed-magazine">
                     <li>
                         <div className="space-y-4">
---
+++ src/pages/power-portfolio.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Gallery from '../components/gallery-main'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { Link } from 'gatsby'
+
+const query = graphql`
+    query indexQueryUV {
+        allFile(filter: {relativePath: {regex: "images/gallery/albums/power-portfolio/"}}, sort: {order: ASC, fields: name}) {
+            edges {
+                node {
+                    childImageSharp {
+                        original {
+                            width
+                            height
+                        }
+                    fluid {
+                        ...GatsbyImageSharpFluid_withWebp
+                        originalName
+                        originalImg
+                    }
+                    }
+                }
+            }
+        }
+    }
+    `
+    const PowerPortfolio = () => {
+    const data = useStaticQuery(query)
+    return (
+        <Layout>
+            <SEO title="UV" />
+            <div className="m-4">
+            <h2 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Power Portfolio</h2>
+            <Link to="/gallery/" className="text-gray-400 italic underline">
+                Back
+            </Link>
+            <Gallery
+                columns={width => {
+                    if (width < 700) {
+                        return 2
+                    } else if (width < 1000) {
+                        return 3
+                    } else {
+                        return 6
+                    }
+                }}
+                photos={data.allFile.edges}
+            />
+            </div>
+        </Layout>
+    )
+}
+
+export default PowerPortfolio
