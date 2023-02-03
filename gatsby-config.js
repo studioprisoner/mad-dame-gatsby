f4d628aea0d316cd80db5e14aad88b433ed97335
Josh Illichmann <josh@studioprisoner.com>
2023-02-03 18:32:51 +1100

    adding old plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,19 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-netlify',
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
