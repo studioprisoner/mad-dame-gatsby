62bf53048f076b411faf9f2840d7bb9481aa7b85
Josh Illichmann <josh@studioprisoner.com>
2023-02-03 23:44:23 +1100

    change to link

--- src/pages/price-list.js
+++ src/pages/price-list.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,9 +9,9 @@ const PriceList = () => (
         <SEO title="Mad Dame Gallery" />
         <div className="bg-white">
             <h1>Price List</h1>
-            <a href={`../static/Mad-Dame-2023-photoshoot-pricelist-compressed.pdf`} title="Download Price List">
+            <Link to={`../static/Mad-Dame-2023-photoshoot-pricelist-compressed.pdf`} title="Download Price List">
                 Download Price List
-            </a>
+            </Link>
         </div>
     </Layout>
 )
