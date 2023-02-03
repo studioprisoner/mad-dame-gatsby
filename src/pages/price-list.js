456d2e797b80027f62155aa2caa2417ffaaab89c
Josh Illichmann <josh@studioprisoner.com>
2023-02-04 00:04:33 +1100

    fix static link

--- src/pages/price-list.js
+++ src/pages/price-list.js
@@ -9,7 +9,7 @@ const PriceList = () => (
         <SEO title="Mad Dame Gallery" />
         <div className="bg-white">
             <h1>Price List</h1>
-            <Link to={`../static/Mad-Dame-2023-photoshoot-pricelist-compressed.pdf`} title="Download Price List">
+            <Link to={`./static/Mad-Dame-2023-photoshoot-pricelist-compressed.pdf`} title="Download Price List">
                 Download Price List
             </Link>
         </div>
