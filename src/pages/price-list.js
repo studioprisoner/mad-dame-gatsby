838afe369c0611c41a2140c046e2b1e039507166
Josh Illichmann <josh@studioprisoner.com>
2023-02-03 22:02:58 +1100

    fixed tailwind purge, and re-added pricelist

---
+++ src/pages/price-list.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PriceList = () => {
+    <Layout>
+        <SEO title="Mad Dame Gallery" />
+        <div className="bg-white">
+            <h1>Price List</h1>
+            <a href={`../Mad-Dame-2023-photoshoot-pricelist-compressed.pdf`} title="Download Price List">
+                Download Price List
+            </a>
+        </div>
+    </Layout>
+}
